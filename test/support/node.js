63532b51abaf3bda65e57eaa3d9f5468c9a4e944
Stepan Stolyarov <stepan@vitotechnology.com>
2014-06-24 14:45:15 +0700

    Use `sinon-chai`

--- test/support/node.js
+++ test/support/node.js
@@ -2,16 +2,20 @@
 
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
 var Promise = require("bluebird");
 
 chai.should();
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
 
 global.chaiAsPromised = chaiAsPromised;
 global.expect = chai.expect;
 global.AssertionError = chai.AssertionError;
 global.Assertion = chai.Assertion;
 global.assert = chai.assert;
+global.sinon = sinon;
 
 global.Promise = Promise;
 global.fulfilledPromise = Promise.resolve;
